6be63b7779439b582e9af1f37ac4871b75195303
Filipe Pereira de Farias <filipepfarias@fisica.ufc.br>
2018-09-06 11:32:24 -0300

    -Created dataset_generator package

---
+++ bayesian_linear_regression/dataset_generator.py
@@ -0,0 +1,32 @@
+def dataset_generator(init, end, sample, mean, var, function):
+    """
+    Return X,Y,t being:
+    x input
+    y deterministic output
+    t data with noise
+    """
+    import numpy as np
+    sample = int(sample)
+    x = (np.array([np.linspace(init,end,sample)])).T
+    y = (function(x))
+    e = (np.array([np.random.normal(mean,var,sample)])).T
+    t = y + e
+
+    return x,y,t,e
+
+def linear_regressor(x,t,function,m):
+    """
+    Return the model trained
+    """
+    import numpy as np
+    phi = np.zeros((len(x),m))
+
+    for i in range(len(x)):
+        for j in range(m):
+            phi[i][j] = function(x[i],j)
+
+    w = ((np.linalg.inv(phi.T@phi))@phi.T)@t
+
+    y_pred = w.T@phi.T
+
+    return y_pred,w
