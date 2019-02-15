2485951d52580002f02edd9d2559304ffb0c698a
Filipe <filipepfarias@fisica.ufc.br>
2019-02-15 15:19:31 -0300

    Fri Feb 15 15:19:31 -03 2019

---
+++ bayesian_linear_regression/python/bayesian_regression.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+from generator import gen3D
+
+X1, X2, Z, T = gen3D()
+
+#Plot
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+# Plot the surface.
+surf = ax.plot_surface(X1, X2, T)
+
+plt.show()
---
+++ bayesian_linear_regression/python/generator.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+def gen3D(**kwargs):
+    if ('initial' in kwargs):
+        initial = kwargs['initial']
+    else:
+        initial = 0
+
+    if ('final' in kwargs):
+        final = kwargs['final']
+    else:
+        final = 1
+
+    if ('ns' in kwargs):
+        ns = kwargs['ns']
+    else:
+        ns = 100
+
+    if ('mean' in kwargs):
+        mean = kwargs['mean']
+    else:
+        mean = 0.0
+
+    if ('sigma' in kwargs):
+        mean = kwargs['sigma']
+    else:
+        sigma = 0.1**2
+
+    x1_lim , x2_lim = np.linspace(initial,final,ns) , np.linspace(initial,final,ns)
+    
+    X1 , X2 = np.meshgrid(x1_lim,x2_lim)
+    
+    Z = np.sin(2*np.pi*X1)*np.sin(2*np.pi*X2)
+
+    E = np.random.normal(mean,sigma,Z.shape)
+    T = Z + E
+    return X1, X2, Z, T
