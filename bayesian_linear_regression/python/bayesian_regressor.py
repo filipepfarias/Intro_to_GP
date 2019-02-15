6dc7c558c14598045c53e9f3c990db38d45c8d2e
Filipe <filipepfarias@fisica.ufc.br>
2019-02-15 16:42:17 -0300

    Fri Feb 15 16:42:17 -03 2019

--- bayesian_linear_regression/python/bayesian_regressor.py
+++ bayesian_linear_regression/python/bayesian_regressor.py
@@ -1,11 +1,16 @@
 import numpy as np
 
-def kernel(**kwargs):
+def kernel(M,*args,**kwargs):
+
+    if ('gaussian' in kwargs):
+
 
     if ('gaussian' in kwargs):
         if not ('M' in kwargs):
             return "There's no M value!"
         else:
             M = kwargs['M']
+    else:
+        return "There's no kernel function!"
 
-            
+return "No parameters given!"
