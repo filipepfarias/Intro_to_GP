915b2ec464068c52d2833e4a2d54cde6ccd16d70
Filipe Farias <filipepfarias@fisica.ufc.br>
2019-02-19 02:10:57 -0300

    `date`

--- bayesian_linear_regression/python/bayesian_regressor.py
+++ bayesian_linear_regression/python/bayesian_regressor.py
@@ -16,7 +16,7 @@ def kernel(M,*args,**kwargs):
             var = np.linspace(PHI1[0,0],PHI1[-1:0],int(sqrt(M))) , np.linspace(PHI2[0,0],PHI2[-1:0],int(sqrt(M)))
             var(0),var(1) = np.meshgrid(var(0),var(1)) 
             mu = np.array([,np.linspace(PHI2[0,0],PHI2[-1:0,int(sqrt(M))])
-
+            
     else:
         return "There's no kernel function!"
 
