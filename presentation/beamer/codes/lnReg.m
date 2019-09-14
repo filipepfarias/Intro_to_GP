e89992a5ad6022f199bb60245f2d3f509e79c4d6
Filipe Farias <filipepfarias@fisica.ufc.br>
2019-09-14 12:22:59 -0300

    Presentation

--- presentation/beamer/codes/lnReg.m
+++ presentation/beamer/codes/lnReg.m
@@ -1,4 +1,4 @@
-clear; close all; warning('off','all'); Wmod = [];
+clear; close all; warning('off','all'); %Wmod = [];
 
 n = 20; 
 x = linspace(0,1,n)';
@@ -8,26 +8,33 @@ t = y(x) + e;
 
 Xp = linspace(x(1),x(end),200)';
 figure;
+style = ['scale=0.075\linewidth,'...
+                       'legend style={nodes={scale=0.5, transform shape}},',...
+                       ];
 
-for M = 1:20
+for M = 1:100
     plot(Xp,y(Xp),'-');hold on; plot(x,t,'o');
     phi = @(a)(bsxfun(@power,a,0:M-1));
     phix = phi(x);
     
     W = ((phix'*phix)\phix')*t;
     
-    hold on; Wmod = [Wmod; log(norm(W))];
+    hold on; %Wmod = [Wmod; log(norm(W))];
     phiXp = phi(Xp); Tp = phiXp*W;
     plot(Xp,Tp); axis([0 1 -1.5 1.5]); xlabel('x','Interpreter','latex'); ylabel('y','Interpreter','latex');
-    legend('Deterministic', 'Data', 'Predicted'); hold off; %pause(0.5);
+    legend('Deterministic', 'Data', 'Predicted'); title(['$M=$ ',num2str(M)],'Interpreter','latex');
+    hold off; %pause(0.1);
+    matlab2tikz([mfilename,'/',mfilename,'_','frame_',num2str(M),'.tex'],'width','0.075\linewidth',...
+        'showInfo', false,'extraaxisoptions', style);
+
 end
-warning('on','all'); print(mfilename,'-depsc');
-style = ['scale=\fwidth,'...
-                       'legend style={nodes={scale=0.5, transform shape}},',...
-                       ];
-matlab2tikz([mfilename,'.tex'],'width','\fwidth',...
-    'showInfo', false,'extraaxisoptions', style); close all;
-figure; semilogy([0:M-1]',Wmod); xlabel('M','Interpreter','latex'); ylabel('\bf ||w_{ML}||'); grid on;
-warning('on','all'); print([mfilename,'-Wmod'],'-depsc');
-matlab2tikz([mfilename,'-Wmod','.tex'],'width','\fwidth',...
-    'showInfo',false,'extraaxisoptions',style);
+%warning('on','all'); print(mfilename,'-depsc');
+% style = ['scale=\fwidth,'...
+%                        'legend style={nodes={scale=0.5, transform shape}},',...
+%                        ];
+% matlab2tikz([mfilename,'.tex'],'width','\fwidth',...
+%     'showInfo', false,'extraaxisoptions', style); close all;
+% figure; semilogy([0:M-1]',Wmod); xlabel('M','Interpreter','latex'); ylabel('\bf ||w_{ML}||'); grid on;
+% warning('on','all'); print([mfilename,'-Wmod'],'-depsc');
+% matlab2tikz([mfilename,'-Wmod','.tex'],'width','\fwidth',...
+%     'showInfo',false,'extraaxisoptions',style);
--- presentation/beamer/codes/lnRegRegulated.m
+++ presentation/beamer/codes/lnRegRegulated.m
@@ -8,18 +8,26 @@ t = y(x) + e;
 
 Xp = linspace(x(1),x(end),200)';
 figure;
+style = ['scale=0.075\linewidth,'...
+                       'legend style={nodes={scale=0.5, transform shape}},',...
+                       ];
 
-for lambda = 75:-1:1
+lnlambda = -[linspace(1e6,1e5,25), linspace(5e1,1,25+30), linspace(1,0,20)];
+
+for i = 1:100
     M = n;
     plot(Xp,y(Xp),'-');hold on; plot(x,t,'o');
     phi = @(a)(bsxfun(@power,a,0:M-1));
     phix = phi(x);
     
-    W = ((phix'*phix+exp(-lambda)*eye(n))\phix')*t;
+    W = ((phix'*phix+exp(lnlambda(i))*eye(n))\phix')*t;
     
     hold on;
     phiXp = phi(Xp); Tp = phiXp*W;
     plot(Xp,Tp); axis([0 1 -1.5 1.5]); xlabel('x','Interpreter','latex'); ylabel('y','Interpreter','latex');
-    legend('Deterministic', 'Data', 'Predicted'); hold off; pause(0.05);
+    legend('Deterministic', 'Data', 'Predicted'); title(['$\ln \lambda = $',num2str(lnlambda(i))],'Interpreter','latex')
+    hold off; pause(0.005);
+    matlab2tikz([mfilename,'/',mfilename,'_','frame_',num2str(i),'.tex'],'width','0.075\linewidth',...
+        'showInfo', false,'extraaxisoptions', style);
 end
-warning('on','all'); print(mfilename,'-depsc');
+warning('on','all'); %print(mfilename,'-depsc');
